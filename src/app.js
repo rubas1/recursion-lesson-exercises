37c953ccfee8e32cddfc2fccfdf1ccff93df27a7
ruba <rubsaleh1@gmail.com>
2020-12-15 11:44:09 +0200

    recursion finished

--- src/app.js
+++ src/app.js
@@ -4,24 +4,32 @@
 */
 
 //Exercise 1
-const findFactorial = function() {
-  //Your code here
-
+const findFactorial = function(n) {
+  if(n === 1){
+    return n
+  }
+  return n*findFactorial(n-1)
 }
 
 //Exercise 2
-const reverseString = function() {
-  //Your code here
-
+const reverseString = function(str, n) {
+  if(n === 1){
+    return str[0]
+  }
+  return str[n-1] + reverseString(str, n-1)
 }
 
 //Exercise 3
 const arr1 = [1, 2, 3]
 const arr2 = []
 
-const swap = function() {
-  //Your code here
-
+const swap = function(arr1, arr2) {
+  if(arr1.length === 0){
+    return 
+  }
+  arr2.push(arr1[0])
+  arr1.shift()
+  return swap(arr1, arr2)
 }
 
 /* DO NOT REMOVE THE EXPORTS BELOW */
